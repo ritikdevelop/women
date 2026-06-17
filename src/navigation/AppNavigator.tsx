1d3f6165699b4956c8c168b93cf4c37f146227ec
ritikdevelop <hrithikrana216@gmail.com>
2026-06-17 14:08:08 +0530

    chore: update dependencies and add AppNavigator structure
    - Updated package.json to include new dependencies for React Navigation and other libraries.
    - Added AppNavigator.tsx file with initial setup for Bottom Tab and Stack Navigators.

---
+++ src/navigation/AppNavigator.tsx
@@ -0,0 +1,17 @@
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+
+const Stack = createNativeStackNavigator();
+
+const Tabs = createBottomTabNavigator();
+
+
+
+// Auth Stack Navigator
+
+
+// Main App Stack Navigator
+
+
+// Bottom Tab Navigator
