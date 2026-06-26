50554fe804e61e965a79e59db4ce6da08d38c6ae
ritikdevelop <hrithikrana216@gmail.com>
2026-06-26 15:42:30 +0530

    created a new screen for AI health coach

---
+++ src/screens/main/AICoachScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const AICoachScreen = () => {
+  return (
+    <View>
+      <Text>AICoachScreen</Text>
+    </View>
+  )
+}
+
+export default AICoachScreen
+
+const styles = StyleSheet.create({})
